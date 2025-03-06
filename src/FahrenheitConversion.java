0ac806afaee97c178146ff0ee029c768fa6ae191
MatheusFreitasGabriel <matheusfreitasdev@hotmail.com>
2025-03-06 19:01:00 -0300

    Conversor de graus para Fahrenheit
    Atividade da aula 2, adicionar um conversor de ceusius para fahrenheit

---
+++ src/FahrenheitConversion.java
@@ -0,0 +1,7 @@
+public class FahrenheitConversion {
+    public static void main(String[] args) {
+        double ceusius = 32;
+        double fahrenheit = (ceusius * 1.8) + 32;
+        System.out.println(fahrenheit);
+    }
+}
